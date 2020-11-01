61d58f308727a3d648190bea99f126dedaba7283
Yan P Aung <yan.pye.aung@gmail.com>
2020-10-31 18:58:14 -0700

    Show FlatList on ListScreen

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const ListScreen = () => {
-  return <Text>List Screen</Text>;
+  const friends = [
+    { name: 'Friend #1', age: 20 },
+    { name: 'Friend #2', age: 45 },
+    { name: 'Friend #3', age: 34 },
+    { name: 'Friend #4', age: 32 },
+    { name: 'Friend #5', age: 31 },
+    { name: 'Friend #6', age: 19 },
+    { name: 'Friend #7', age: 21 },
+    { name: 'Friend #8', age: 24 },
+    { name: 'Friend #9', age: 22 }
+  ];
+  return (
+    <FlatList
+      keyExtractor={friend => friend.name}
+      data={friends}
+      renderItem={({ item }) => {
+        return <Text style={styles.textStyle}>{item.name} - Age: {item.age}</Text>;
+      }}
+    />
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textStyle: {
+    marginVertical: 50
+  }
+});
 
 export default ListScreen;

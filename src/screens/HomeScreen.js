06e6183b51151f4d59a02ebd840e3be3038ee6de
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-01 19:35:42 -0800

    Refactor to destructure only navigation prop

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = (props) => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.text}>Hi there!</Text>
       <Button
-        onPress={() => props.navigation.navigate('Components')}
+        onPress={() => navigation.navigate('Components')}
         title="Go to Components Demo"
       />
       <Button
         title="Go to List Demo"
-        onPress={() => props.navigation.navigate('List')}
+        onPress={() => navigation.navigate('List')}
       />
     </View>
   );

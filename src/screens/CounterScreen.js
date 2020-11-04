e4a3a2bfaf250a06ccc357fcfd087b58f0176eeb
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-03 20:09:14 -0800

    Make CounterScreen work by using useState hooks

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
 const CounterScreen = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <View>
-      <Text>Current Count:</Text>
+      <Button title="Increase" onPress={() => {
+        // Don't modify directly, use hook
+        // counter++;
+        setCounter(counter + 1);
+      }} />
+      <Button title="Decrease" onPress={() => {
+        setCounter(counter - 1);
+      }} />
+      <Text>Current Count: {counter} </Text>
     </View>
   );
 }

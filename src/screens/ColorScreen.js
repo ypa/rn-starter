85df2afc5a49a5a26a854bb4b243928854035ab7
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-04 20:30:48 -0800

    Render colors with FlatList component

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Button } from 'react-native';
+import { View, StyleSheet, Button, FlatList } from 'react-native';
 
 const ColorScreen = () => {
   const [colors, setColors] = useState([]);
@@ -10,7 +10,13 @@ const ColorScreen = () => {
       <Button title="Add a Color" onPress={() => {
         setColors([...colors, randomRgb()]);
       }}/>
-      <View style={{ height: 100, width: 100, backgroundColor: randomRgb() }} />
+      <FlatList
+        keyExtractor={(item) => item}
+        data={colors}
+        renderItem={({ item }) => {
+          return <View style={{ height: 100, width: 100, backgroundColor: item }} />
+        }}
+      />
     </View>
   );
 };

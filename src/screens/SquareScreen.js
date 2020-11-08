eace3c8b8e291ebde593a8010f749216b9145a5e
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-07 20:35:14 -0800

    Add validation for color range logic back in place

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -13,11 +13,17 @@ const reducer = (state, action) => {
       // never change the state object, ie.,
       // never going to do:
       // state.red = state.red - 15;
-      return { ...state, red: state.red + action.amount };
+      return (state.red + action.amount > 255 || state.red + action.amount < 0)
+        ? state
+        : { ...state, red: state.red + action.amount };
     case 'green':
-      return { ...state, green: state.green + action.amount };
+      return (state.green + action.amount > 255 || state.green + action.amount < 0)
+        ? state
+        : { ...state, green: state.green + action.amount };
     case 'blue':
-      return { ...state, blue: state.blue + action.amount };
+      return (state.blue + action.amount > 255 || state.blue + action.amount < 0)
+        ? state
+        : { ...state, blue: state.blue + action.amount };
     default:
       return state;
   }

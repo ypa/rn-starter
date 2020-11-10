7f025df928ac675afd9547f45c80c8d019273a7b
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-09 19:59:53 -0800

    Add an example of Flex box model with alignItems flex-end

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,7 +4,9 @@ import { Text, View, StyleSheet } from 'react-native';
 const BoxScreen = () => {
   return (
     <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>Box Screen</Text>
+      <Text style={styles.textStyle}>Child #1</Text>
+      <Text style={styles.textStyle}>Child #2</Text>
+      <Text style={styles.textStyle}>Child #3</Text>
     </View>
   );
 };
@@ -12,12 +14,12 @@ const BoxScreen = () => {
 const styles = StyleSheet.create({
   viewStyle: {
     borderWidth: 3,
-    borderColor: 'black'
+    borderColor: 'black',
+    alignItems: 'flex-end'
   },
   textStyle: {
-    borderWidth: 10,
-    borderColor: 'red',
-    margin: 20
+    borderWidth: 3,
+    borderColor: 'red'
   }
 });
 

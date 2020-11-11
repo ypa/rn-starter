04912e8ae6a72a9fa9cfe070ee752cd4d9891922
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-10 19:17:54 -0800

    Add 3 box layout exercise

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,33 +3,37 @@ import { Text, View, StyleSheet } from 'react-native';
 
 const BoxScreen = () => {
   return (
-    <View style={styles.viewStyle}>
-      <Text style={styles.textOneStyle}>Child #1</Text>
-      <Text style={styles.textTwoStyle}>Child #2</Text>
-      <Text style={styles.textThreeStyle}>Child #3</Text>
+    <View style={styles.parentStyle}>
+      <View style={styles.viewOneStyle} />
+      <View style={styles.viewTwoStyle} />
+      <View style={styles.viewThreeStyle} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  viewStyle: {
+  parentStyle: {
     borderWidth: 3,
     borderColor: 'black',
-    height: 200
+    height: 200,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
   },
-  textOneStyle: {
-    borderWidth: 3,
-    borderColor: 'red'
+  viewOneStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'red'
   },
-  textTwoStyle: {
-    borderWidth: 3,
-    borderColor: 'red',
-    fontSize: 18,
-    ...StyleSheet.absoluteFillObject
+  viewTwoStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'green',
+    marginTop: 50
   },
-  textThreeStyle: {
-    borderWidth: 3,
-    borderColor: 'red'
+  viewThreeStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'purple'
   }
 });
 
